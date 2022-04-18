04b15052e1d05ae84eb927b9c3d059ca0d78b346
Ryan Jussel <rwjussel@gmail.com>
2022-04-17 20:32:19 -0500

    Added Gift information

--- src/@agney/gatsby-theme-wedding/components/CoverText.js
+++ src/@agney/gatsby-theme-wedding/components/CoverText.js
@@ -9,6 +9,12 @@ import animationParams from "@agney/gatsby-theme-wedding/src/components/animatio
 const Text = styled.span`
   margin: 1rem;
   font-size: 1.5rem;
+  
+  ${media.phone`
+    margin: 0 0 1rem 0;
+    display: flex;
+    flex-direction: column;
+  `}
 `;
 
 const Heading = styled.h1`
@@ -20,7 +26,7 @@ const Heading = styled.h1`
   }
 
   ${media.phone`
-    margin: 1rem 0;
+    margin: 0 0 12rem 0;
     display: flex;
     flex-direction: column;
   `}
@@ -33,7 +39,24 @@ const ImpText = styled(Text)`
     color: ${props => props.theme.colors.light};
     font-weight: 600;
   `}
+
 `;
+
+const FaqText= styled(Text)`
+  color: ${props => props.theme.colors.primary};
+  background: ${props => props.theme.colors.light};
+  font-weight: 400;
+  font-size: 2rem;
+
+  ${media.phone`
+  border: 2px solid black;
+  padding: 25px;
+  margin: 0 0 0 18%;
+  display: flex;
+  width: 50%;
+  flex-direction: column;
+`}
+`
 
 const RSVPText= styled(Text)`
   color: ${props => props.theme.colors.primary};
@@ -82,11 +105,12 @@ function CoverText() {
         {event.events[0].title}
       </ImpText>
       <Text as="p">in</Text>
-      <ImpText as="p" {...animationParams}>
+      <ImpText as="p" {...animationParams} data-sal-delay="100">
         Fort Collins, Colorado
       </ImpText>
-      <RSVPText as="p" {...animationParams} > <a href="https://forms.gle/bmuFPHouDTgg1CMV8">R.S.V.P. Now!</a> </RSVPText>
-      <ImpText as="p" {...animationParams} > Questions? See the <a href="https://wedding.ryanjillian.com/faq">FAQ</a> </ImpText>
+      <RSVPText as="p" {...animationParams} data-sal-delay="100"> <a href="https://forms.gle/bmuFPHouDTgg1CMV8">R.S.V.P. Now!</a> </RSVPText>
+      <ImpText as="p" {...animationParams} data-sal-delay="100">   We kindly request no gifts for our wedding. Your journey to celebrate with us is enough of a gift.  If you insist on giving we have selected charities that you may donate to on our behalf. </ImpText>
+      <FaqText as="p" {...animationParams} data-sal-delay="100"> Questions? See the <a href="https://wedding.ryanjillian.com/faq" >FAQ</a> </FaqText>
     </Fragment>
   );
 }
